9ce18594c3a5046a5f98f1dd57aeabb8069f3a48
Adriano Parente <adrianopmachado@outlook.com>
2022-10-21 19:03:08 -0300

    Commit da galeria em JS

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 // Ativar links do menu
+
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -13,6 +14,7 @@ function ativarLink(link) {
 links.forEach(ativarLink);
 
 // Ativar items do orçamento
+
 const parametros = new URLSearchParams(location.search);
 
 function ativarProduto(parametro) {
@@ -25,6 +27,7 @@ function ativarProduto(parametro) {
 parametros.forEach(ativarProduto);
 
 //Perguntas Frequentes
+
 const perguntas = document.querySelectorAll(".perguntas button");
 
 function ativarPergunta(event) {
@@ -42,3 +45,22 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+//Galeria de Bicicletas
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 920px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
